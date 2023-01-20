798ffc9479a6b1866808209f2bf27e8c99f91f9e
pariparhat <pparizat21@gmail.com>
2023-01-20 14:46:37 -0600

    second commit

---
+++ src/test/java/orangehrmproject/NewTest.java
@@ -0,0 +1,13 @@
+package orangehrmproject;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  private void sysout() {
+	// TODO Auto-generated method stub
+
+}
+  public void f() {
+  }
+}
